3624933a50bf62b69119e8da1a030845cf5df49d
Larisa Kosareva <kos.lv@yandex.com>
2021-12-09 15:33:35 +0100

    day9 user registration

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 from .models import Profile
 from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
 def loginUser(request):
+    page = 'register'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -34,6 +37,26 @@ def logoutUser(request):
     logout(request)
     messages.error(request, "User was logged out")
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, "An error has occurred during the registaration")
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
