14cd702eea19a2cecfa5f1444c6ecb7acaed1977
Larisa Kosareva <kos.lv@yandex.com>
2021-12-13 22:22:50 +0100

    day 11 User account

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -65,7 +65,19 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
+
     topSkills = profile.skill_set.exclude(description__exact="")
     otherSkills = profile.skill_set.filter(description="")
+
     context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {'profile': profile, 'skills': skills, 'projects': projects }
+    return render(request, 'users/account.html', context)
