ca1be199803fead4f35e30a764363352faf357f0
Larisa Kosareva <kos.lv@yandex.com>
2021-12-14 14:50:23 +0100

    day12 user account edit form

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -13,8 +13,18 @@ def createProfile(sender, instance, created, **kwargs):
             user=user,
             username=user.username,
             email=user.email,
-            name=user.first_name
+            name=user.first_name,
         )
+
+def updateProfile(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
 
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
@@ -22,5 +32,6 @@ def deleteUser(sender, instance, **kwargs):
     print('Deleting user ')
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateProfile, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
 
