3d8a1c68c9647604b28a6fa92b6e15e613489ffd
Larisa Kosareva <kos.lv@yandex.com>
2021-12-16 14:29:42 +0100

    day 14 search

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -3,10 +3,10 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Profile
+from .models import Profile, Skill
 from django.contrib.auth.models import User
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
-
+from django.db.models import Q
 
 # Create your views here.
 def loginUser(request):
@@ -59,8 +59,20 @@ def registerUser(request):
     return render(request, 'users/login_register.html', context)
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    search_query = ''
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+    print('SEARCH:', search_query)
+
+    skills = Skill.objects.filter(name__icontains=search_query)
+
+    profiles = Profile.objects.distinct().filter(
+        Q(name__icontains=search_query) | 
+        Q(short_info__icontains=search_query) | 
+        Q(skill__in=skills)
+        )
+    context = {'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
 def userProfile(request, pk):
