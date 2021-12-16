dfd0887c70514feb9c66edc333fdd94944969444
Larisa Kosareva <kos.lv@yandex.com>
2021-12-16 14:53:40 +0100

    day 14 refactoring search engine

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Profile, Skill
+from .models import Profile
 from django.contrib.auth.models import User
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
-from django.db.models import Q
+from .utils import searchProfiles
+
 
 # Create your views here.
 def loginUser(request):
@@ -59,19 +60,7 @@ def registerUser(request):
     return render(request, 'users/login_register.html', context)
 
 def profiles(request):
-    search_query = ''
-
-    if request.GET.get('search_query'):
-        search_query = request.GET.get('search_query')
-    print('SEARCH:', search_query)
-
-    skills = Skill.objects.filter(name__icontains=search_query)
-
-    profiles = Profile.objects.distinct().filter(
-        Q(name__icontains=search_query) | 
-        Q(short_info__icontains=search_query) | 
-        Q(skill__in=skills)
-        )
+    profiles, search_query = searchProfiles(request)
     context = {'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
