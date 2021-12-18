1e982ea83edbbdbb85c01b25c670418f8e2f519a
Larisa Kosareva <kos.lv@yandex.com>
2021-12-18 18:21:35 +0100

    day 15 projects pagination

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Project
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required
@@ -10,6 +10,19 @@ from .utils import searchProjects
 
 def projects(request):
     projects, search_query = searchProjects(request)
+
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
 
     context = {'projects': projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context )
