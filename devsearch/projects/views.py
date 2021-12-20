380f2698f323a969bdd1b96c7c8a6ec37bd2d6d2
Larisa Kosareva <kos.lv@yandex.com>
2021-12-20 14:43:31 +0100

    day18 reviews

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib import messages
 from .models import Project
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from django.contrib.auth.decorators import login_required
 from .utils import searchProjects, paginateProjects
 
@@ -17,13 +17,29 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
+    form = ReviewForm()
+    print("projectObj", projectObj.reviewers)
     
-    return render(request, 'projects/project.html', {'project': projectObj})
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        # update  project vote count
+        projectObj.getVoteCount
+
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/project.html', {'project': projectObj, 'form': form})
 
 @login_required(login_url='login')
 def createProject(request):
     profile = request.user.profile
     form = ProjectForm()
+    
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
