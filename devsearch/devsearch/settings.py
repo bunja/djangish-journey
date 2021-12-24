f72b79c4bee00934a2599df3b08c4d9359d69d88
Larisa Kosareva <kos.lv@yandex.com>
2021-12-24 20:18:37 +0100

    day 23 django rest and serializers

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'projects.apps.ProjectsConfig',
     'users.apps.UsersConfig',
+
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
