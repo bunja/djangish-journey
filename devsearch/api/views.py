30d27f58cb37f2656bbd61d4a2f58ea4eac3f84c
Larisa Kosareva <kos.lv@yandex.com>
2021-12-24 22:29:17 +0100

    day 24 authentication with json web tokens, restricted routes, customizing tokens

--- devsearch/api/views.py
+++ devsearch/api/views.py
@@ -1,11 +1,14 @@
-
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions  import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from projects.models import Project
 
+from api import serializers
+
 
 @api_view(['GET'])
+
 def getRoutes(request):
     routes = [
         {'GET':'/api/projects/'},
@@ -19,7 +22,9 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProjects(request):
+    print('USER:', request.user)
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
